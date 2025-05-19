5df0df12e9711a5944268df2d96bedc7b9c68345
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-05-19 20:46:09 -0300

    primeiro commit

---
+++ exercicios/ex007/problema.js
@@ -0,0 +1,15 @@
+/*
+1. obter o nome do filme
+2. obter a duração em minutos
+3. converter a duração de minutos em horas
+  3.1 fazer a divisão da duração em minutos por 60 pra pegar a quantidade em horas
+  3.2 arrendondar a quantidade de horas para pegar só as horas cheias
+  3.3 pegar o resultado da duração em minutos e remover as horas cheias (horas * 60)
+    exemplo: 70 minutos (1h e 10 min)
+    60 minutos = 1hr
+    10 minutos
+    70 / 60 = 1,16 (arrendondar pra baixo) = 1
+    70 - 1 * 60 = 10
+4. exibir o nome do filme
+5. exibir a duração em horas e minutos
+*/
