07a2826f18ae8fbead38a5a97f2b891b9734d190
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-08-07 11:58:22 -0300

    webstorage

---
+++ exercicios/api/webstorage/exemplo.js
@@ -0,0 +1,11 @@
+// salvar itens
+localStorage.setItem('chave', valor)
+
+// Recuperar itens
+localStorage.getItem('chave')
+
+// Remover um item
+localStorage.removeItem('chave')
+
+// Limpar tudo
+localStorage.clear()
---
+++ exercicios/api/webstorage/script.js
@@ -0,0 +1,27 @@
+const botaoTema = document.getElementById("botaotema")
+
+botaoTema.addEventListener("click", ()=> {
+    //verificar se ja tem um tema pré definido
+    const temaAtual = localStorage.getItem("tema")
+    //Verificar qual é o tema e inverter
+    const novotema = temaAtual === "dark" ? "light" : "dark"
+
+    //add a classe no elemento body
+    document.body.classList.toggle(novotema)
+
+    //salvar as preferencias
+    localStorage.setItem("item", novotema)
+    botaoTema.textContent = novotema === "dark" ? 'Tema Claro' : 'Tema Escuro'
+})
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const temaSalvo = localStorage.getItem("tema")
+
+    // se for dark, adiciona a classe e altera o botão
+    if (temaSalvo === "dark") {
+        document.body.classList.add("dark")
+        botaoTema.textContent = 'Tema Claro'
+    } else {
+        botaoTema.textContent = 'Tema Escuro'
+    }
+})
